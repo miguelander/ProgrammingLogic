7804fb6118e588a7f95343ecb4cbd8e53c7b548e
miguel anderson onofre <onofremiguel235@gmail.com>
2024-05-22 09:53:56 -0500

    exercise1

--- exercise1.js
+++ exercise1.js
@@ -1,1 +1,73 @@
-console.log("test...")
+/*
+Enunciado Ejercicio 1:
+Dado un array o un objeto de superhéroes de Marvel, haz un programa que pueda
+mostrar la información de un superheroe.
+ 
+Y añade una capacidad de buscar la información de varios superhéroes a la vez.
+ 
+Ejemplo:
+mostrarInformacionSuperheroe('Iron Man');
+ 
+Salida:
+Nombre real: Tony Stark
+Poderes: Tecnología avanzada, Movilidad aérea
+Equipo: Los vengadores
+ 
+Ejemplo 2: 
+mostrarInformacionSuperheroes([array de nombres]]);
+ 
+Salida:
+Muestra la información de todos los superheroes
+ 
+*/
+ 
+const infoSuperheroes = {
+    'Iron Man': {
+        nombreReal: 'Tony Stark',
+        poderes: ['Tecnologia avanzada', 'Movilidad aérea'],
+        equipo: 'Los vengadores'
+    },
+    'Capitán América': {
+        nombreReal: 'Steve Rogers',
+        poderes: ['Fuerza sobrehumana', 'Agilidad y reflejos sobresalientes'],
+        equipo: 'Los vengadores'
+    },
+    'Thor': {
+        nombreReal: 'Thor Odinson',
+        poderes: ['Mjolnir', 'Viento y trueno'],
+        equipo: 'Los vengadores'
+    },
+    'Spider-Man': {
+        nombreReal: 'Peter Parker',
+        poderes: ['Balanceo', 'Telarañas pegajosas', 'Sentido aracnido'],
+        equipo: 'Los vengadores'
+    },
+    'Hulk': {
+        nombreReal: 'Bruce Banner',
+        poderes: ['Fuerza sobrehumana', 'Invulnerabilidad'],
+        equipo: 'Los vengadores'
+    }
+};
+
+
+ 
+const showOneSuperHero = (aka) => {
+    if(aka in infoSuperheroes){
+        const information = infoSuperheroes[aka]
+
+        console.log(`${aka}`, information)
+   
+    }else{
+        console.log(`The super Hero ${aka} not exist`)
+    }
+    
+     }
+
+const showAllSuperHeros = (akas) => {
+    
+    akas.forEach(nombre => {
+        showOneSuperHero(nombre)
+    });
+    
+}
+showAllSuperHeros(["Hulk", "Iron Man", "Batman"])
